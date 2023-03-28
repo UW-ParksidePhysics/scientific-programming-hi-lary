a4f9a0ee4d976bce29a4818b394c3f3a7fb0e87b
hi-lary <beansy7@sbcglobal.net>
2023-03-28 12:02:11 -0500

    homework/practice

--- python/two_balls_bounce.py
+++ python/two_balls_bounce.py
@@ -1,8 +1,11 @@
 import vpython as vp
 
-initial_position = vp.vector(-10., 0., 0.)
-initial_velocity = vp.vector(25., 0., 0.)
-ball = vp.sphere(pos=initial_position, radius=0.5, color=vp.color.blue, make_trail=True)
+initial_position_one = vp.vector(-10., -10, 0.)
+initial_velocity_one = vp.vector(25., 25, 0.)
+ball_one = vp.sphere(pos=initial_position_one, radius=0.5, color=vp.color.blue, make_trail=True)
+initial_position_two = vp.vector(-10., 10, 0.)
+initial_velocity_two = vp.vector(25., -25, 0.)
+ball_two = vp.sphere(pos=initial_position_two, radius=0.5, color=vp.color.yellow, make_trail=True)
 
 wall_center = vp.vector(0., 0., 0.)
 wall_dimensions = vp.vector(0.25, 10., 10.)
@@ -14,12 +17,18 @@ time_step = 0.005
 stop_time = 1.
 
 time = 0.
-ball_velocity = initial_velocity
+ball_velocity_one = initial_velocity_one
+ball_velocity_two = initial_velocity_two
 while time < stop_time:
     vp.rate(rate_of_animation)
-    if ball.pos.x > wall.pos.x:
-        ball_velocity.x = -ball_velocity.x  # reverse ball velocity
-    ball.pos.x += ball_velocity.x * time_step
-    ball.pos.y += ball_velocity.y * time_step
-    ball.pos.z += ball_velocity.z * time_step
+    if ball_one.pos.x > wall.pos.x:
+        ball_velocity_one.x = -ball_velocity_one.x  # reverse ball velocity
+    ball_one.pos.x += ball_velocity_one.x * time_step
+    ball_one.pos.y += ball_velocity_one.y * time_step
+    ball_one.pos.z += ball_velocity_one.z * time_step
+    if ball_two.pos.x > wall.pos.x:
+        ball_velocity_two.x = -ball_velocity_two.x
+    ball_two.pos.x += ball_velocity_two.x * time_step
+    ball_two.pos.y += ball_velocity_two.y * time_step
+    ball_two.pos.z += ball_velocity_two.z * time_step
     time += time_step
--- python/two_balls_cross.py
+++ python/two_balls_cross.py
@@ -1,8 +1,11 @@
 import vpython as vp
 
-initial_position = vp.vector(-1., 0., 0.)
-initial_velocity = vp.vector(1., 0., 0.)
-ball = vp.sphere(pos=initial_position, radius=0.1, color=vp.color.red, make_trail=True)
+initial_position_one = vp.vector(1, 2, 3)
+initial_velocity_one = vp.vector(1, 1, 1)
+ball_one = vp.sphere(pos=initial_position_one, radius=0.1, color=vp.color.yellow, make_trail=True)
+initial_position_two = vp.vector(4, 5, 6)
+initial_velocity_two = vp.vector(-1, -1, -1)
+ball_two = vp.sphere(pos=initial_position_two, radius=0.1, color=vp.color.purple, make_trail=True)
 
 animation_time_step = 0.1  # seconds
 rate_of_animation = 1/animation_time_step
@@ -12,8 +15,12 @@ stop_time = 10.
 time = 0.
 while time < stop_time:
     vp.rate(rate_of_animation)
-    x = initial_position.x + initial_velocity.x * time
-    y = initial_position.y + initial_velocity.y * time
-    z = initial_position.z + initial_velocity.z * time
-    ball.pos = vp.vector(x, y, z)
+    x_one = initial_position_one.x + initial_velocity_one.x * time
+    y_one = initial_position_one.y + initial_velocity_one.y * time
+    z_one = initial_position_one.z + initial_velocity_one.z * time
+    ball_one.pos = vp.vector(x_one, y_one, z_one)
+    x_two = initial_position_two.x + initial_velocity_two.x * time
+    y_two = initial_position_two.y + initial_velocity_two.y * time
+    z_two = initial_position_two.z + initial_velocity_two.z * time
+    ball_two.pos = vp.vector(x_two, y_two, z_two)
     time += time_step
