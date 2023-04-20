ba681880866b543d37f349d94fd1688120b6a0ec
hi-lary <beansy7@sbcglobal.net>
2023-04-20 15:04:07 -0500

    homework/practice

--- Final_Exam_Review/bivariate_statistics.py
+++ Final_Exam_Review/bivariate_statistics.py
@@ -3,4 +3,11 @@ from scipy import stats
 
 
 def bivariate_statistics(data_array):  # data_array is from two_column_text_read
-    statts = stats.stats.describe(data_array, axis=1)
+    statties = stats.stats.describe(data_array, axis=1)
+    mean_y = statties.minmax[0][1]
+    x_min, y_min = statties.minmax[0][0], statties.minmax[0][1]
+    x_max, y_max = statties.minmax[-1][0], statties.minmax[-1][1]
+    standard_deviation_of_y = np.sqrt(statties.variance[1])
+    statistics = np.array([mean_y, standard_deviation_of_y, x_min, x_max, y_min, y_max])
+    return statistics
+
