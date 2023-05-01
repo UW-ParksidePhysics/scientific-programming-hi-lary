f967ff859ff7d204cd1d489323e952f776531d49
hi-lary <beansy7@sbcglobal.net>
2023-05-01 16:56:49 -0500

    homework/practice

--- Final_Exam_Review/plot_data_with_fit.py
+++ Final_Exam_Review/plot_data_with_fit.py
@@ -6,4 +6,11 @@ import fit_curve_array as fit_curve_array
 def plot_data_with_fit(data_array, fit_curve, data_format="", fit_format=""):
     scatter_plot = plt.plot(data_array[0, :], data_array[1, :], data_format)
     curve_plot = plt.plot(fit_curve[0, :], fit_curve[1, :], fit_format)
+    plt.show()
     return scatter_plot, curve_plot
+
+
+if __name__ == "__main__":
+    data = [[-2, -1, 0, 1, 2], [4, 1, 0, 1, 4]]
+    test_curve = [np.linspace(-2, 2), np.linspace(-2, 2) ** 2]
+    print(plot_data_with_fit(data, test_curve))
