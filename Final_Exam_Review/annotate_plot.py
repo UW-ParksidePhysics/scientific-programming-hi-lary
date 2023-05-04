8ce57922ae74e325d61f3093d99dc36f96da832b
hi-lary <beansy7@sbcglobal.net>
2023-05-04 14:25:03 -0500

    homework/practice

--- Final_Exam_Review/annotate_plot.py
+++ Final_Exam_Review/annotate_plot.py
@@ -1,7 +1,23 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 def annotate_plot(annotations):
     # plt.text(x, y, s, fontdict=None, **kwargs)
     # x,y=floats, s=string, fontdict=None or dict, kwargs= other properties
-    plt.text(annotations['positions'][0], annotations['positions'][1], annotations['string'], )
+    plt.text(annotations['position'][0], annotations['position'][1], annotations['string'],
+             horizontalalignment=annotations['alignment'][0], verticalalignment=annotations['alignment'][1],
+             fontsize=annotations['fontsize'])
+    return
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+    import numpy as np
+
+    test_annotations = {'string': f"Created by Hillary {datetime.today().isoformat()}",
+                        'position': np.array([4.75, 9.5]), 'alignment': ['left', 'bottom'], 'fontsize': 10}
+    plt.plot(5, 10)
+    plt.text(x=test_annotations['position'][0], y=test_annotations['position'][1], s=test_annotations['string'],
+             horizontalalignment=test_annotations['alignment'][0], verticalalignment=test_annotations['alignment'][1],
+             fontsize=test_annotations['fontsize'])
+    plt.show()
