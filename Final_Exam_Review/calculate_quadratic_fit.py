f0ac9370dbc1c5998c63000b132c1a3dd9cb1113
hi-lary <beansy7@sbcglobal.net>
2023-05-08 14:28:10 -0500

    homework/practice

--- Final_Exam_Review/calculate_quadratic_fit.py
+++ Final_Exam_Review/calculate_quadratic_fit.py
@@ -5,8 +5,11 @@ from read_two_column_text import read_two_column_text
 def calculate_quadratic_fit(data_array):
     x_values = data_array[0, :]
     y_values = data_array[1, :]
-    quadratic_coefficients = np.polyfit(x_values, y_values, 2)
+    quadratic_coefficients = np.polynomial.polynomial.polyfit(x_values, y_values, 2)
     return quadratic_coefficients
 
 
-print(calculate_quadratic_fit(read_two_column_text('volume_energies')))
+if __name__ == "__main__":
+    data = np.array([np.linspace(-1, 1), np.linspace(-1, 1)**2])
+    print(calculate_quadratic_fit(data))
+    # print(calculate_quadratic_fit(read_two_column_text('volume_energies')))
