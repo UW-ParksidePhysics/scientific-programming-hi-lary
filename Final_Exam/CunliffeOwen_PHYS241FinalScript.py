9fc226caa66422bbb18ce59d60f94b8e186fb467
hi-lary <beansy7@sbcglobal.net>
2023-05-09 18:21:57 -0500

    exam

--- Final_Exam/CunliffeOwen_PHYS241FinalScript.py
+++ Final_Exam/CunliffeOwen_PHYS241FinalScript.py
@@ -7,6 +7,7 @@ from plot_data_with_fit import plot_data_with_fit
 from annotate_plot import annotate_plot
 from equations_of_state import *
 from convert_units import convert_units
+import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
@@ -24,18 +25,29 @@ if __name__ == "__main__":
     two_column_data = (read_two_column_text(filename)) / 2
     statistical_data = calculate_bivariate_statistics(two_column_data)
     quad_fit_data = calculate_quadratic_fit(two_column_data)
-    fit_eos_curve, fit_eos_parameters = fit_eos(statistical_data[0], statistical_data[1],
+
+
+    fit_eos_curve, fit_eos_parameters = fit_eos(two_column_data[0], two_column_data[1],
                                                 quad_fit_data, eos='birch-murnaghan')
     x_fit_curve = fit_curve_array(quad_fit_data, statistical_data[2], statistical_data[3], number_of_points=50)
-    volume_list1 = [convert_units(volume, 'bohr/atom', 'angstrom**3/atom') for volume in x_fit_curve[0]]
-    energy_list1 = [convert_units(energy, 'rydberg/atom', 'eV/atom') for energy in x_fit_curve[1]]
-    bulk_modulus = convert_units(eos_parameters[1], 'rydberg/bohr**3', 'gigapascals')
 
-    volume_list2 = [convert_units(volume, 'bohr/atom', 'angstrom**3/atom') for volume in two_column_data[0]]
-    energy_list2 = [convert_units(energy, 'rydberg/atom', 'eV/atom') for energy in two_column_data[1]]
+    volume_list1 = [convert_units(x_fit_curve[0], 'bohr/atom', 'angstrom**3/atom')]
+    energy_list1 = [convert_units(x_fit_curve[1], 'rydberg/atom', 'eV/atom')]
+    bulk_modulus = convert_units(fit_eos_parameters[1], 'rydberg/bohr**3', 'gigapascals')
+
+    volume_list2 = [convert_units(two_column_data[0], 'bohr/atom', 'angstrom**3/atom')]
+    energy_list2 = [convert_units(two_column_data[1], 'rydberg/atom', 'eV/atom')]
 
     plt.plot(volume_list1, energy_list1, 'black')
     plt.plot(volume_list2, energy_list2, 'bo')
 
-    plt.show()
+    x_range = max(volume_list1) - min(volume_list1)
+    y_range = max(energy_list1) - min(energy_list1)
+    x_limits = [min(volume_list1) - (0.1 * x_range), max(volume_list1) + (0.1 * x_range)]
+    y_limits = [min(energy_list1) - (0.1 * y_range), max(energy_list1) + (0.1 * y_range)]
 
+    plt.xlim(x_limits[0], x_limits[1])
+    plt.ylim(y_limits[0], y_limits[1])
+   # plt.xlabel(r'$V\/(\mathrm{Å^3/atom})$')
+   # plt.ylabel(r'$E\/(\mathrm{eV/atom})$')
+    plt.show()
