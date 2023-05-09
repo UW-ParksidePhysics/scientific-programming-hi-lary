3415a43c2ae3fc538a343adcce3674972a3e608a
hi-lary <beansy7@sbcglobal.net>
2023-05-09 12:25:14 -0500

    homework/practice

--- Final_Exam/CunliffeOwen_PHYS241FinalScript.py
+++ Final_Exam/CunliffeOwen_PHYS241FinalScript.py
@@ -3,18 +3,33 @@
 "from Final_Exam_Review import calculate_lowest_eigenvectors, fit_curve_array, plot_data_with_fit"
 "from Final_Exam_Review import annotate_plot"
 import os
+import numpy as np
 
 # infile = r"C:\\Users\\cunli003\\Downloads\\Sn.Fd-3m.GGA-PBE.volumes_energies.dat"
 # head, tail = os.path.split(infile)
 # print(head, tail)
 
 if __name__ == "__main__":
+    filename = 'Sn.Fd-3m.GGA-PBE.volumes_energies.dat'
+
+
     def parse_file_name():
-        filename = 'Sn.Fd-3m.GGA-PBE.volumes_energies.dat'
         filename_split = filename.split(".")[0:3]
-        # infile = open(filename, 'r')
-        # print(filename_split)
         return filename_split
 
 
-    print(parse_file_name())
+    def make_data_arrays():
+        infile = open(filename, 'r').readlines()
+        # 1st column is volumes_cells, 2nd column is total_energies
+        volumes_cells = np.array([])
+        total_energies = np.array([])
+        for line in infile:
+            words = line.split()
+            first = float(words[0][0:-4])*10**1
+            second = float(words[1][0:-4])*10**3
+            volumes_cells = np.append(volumes_cells, first)
+            total_energies = np.append(total_energies, second)
+        return [volumes_cells, total_energies]
+
+
+    print(make_data_arrays())
